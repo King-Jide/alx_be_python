08539b32622aa5f6b5d9c95dbcf83ca112212ef0
David Jideofor <jide.remote@gmail.com>
2025-07-16 14:07:09 +0100

    sytax fix on line 2, current age

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #User Input Age Calculator
-current_age = int(input("How old are you? ."))
+current_age = int(input("How old are you?"))
 future_age = current_age + 27
 #print the age in 2050 (27 years from now)
 print(f"In 2050, you will be {future_age} years old.")
