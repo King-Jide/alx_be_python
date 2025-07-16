1ea6e05c23c77bab6782df7b857024eac092e77f
David Jideofor <jide.remote@gmail.com>
2025-07-16 13:41:59 +0100

    update to the simple interest calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,20 +1,22 @@
 # This program calculates simple interest
 principal = 1000
+rate = 0.05
 time = 3
-rate = float(0.05)
-def calculate_simple_interest(principal, time, rate):
-    """
-    Calculate simple interest.
 
-    Parameters:
-    principal (float): The principal amount.
-    time (float): The time in years.
-    rate (float): The rate of interest per year.
+# def calculate_simple_interest(principal, time, rate):
+#     """
+#     Calculate simple interest.
 
-    Returns:
-    float: The calculated simple interest.
-    """
-    return principal * time * rate  
+#     Parameters:
+#     principal (float): The principal amount.
+#     time (float): The time in years.
+#     rate (float): The rate of interest per year.
 
-interest = calculate_simple_interest(principal, time, rate)
+#     Returns:
+#     float: The calculated simple interest.
+#     """
+#     return principal * time * rate  
+
+
+interest = principal * time * rate
 print("The simple interest is:", interest)
