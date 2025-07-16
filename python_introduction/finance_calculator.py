b4dfae90e6734132c563e3d2f652f2b0247bdc05
David Jideofor <jide.remote@gmail.com>
2025-07-16 14:30:14 +0100

    syntax fix

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 #personal finance calculator
 #User to input their monthly income and expenses
 monthly_income = float(input("Enter your monthly income: "))
-total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 #Calculate the monthly savings
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 #Calculate Projected Annual Savings
 projected_annual_savings = (monthly_savings * 12) + (monthly_savings *12 * 0.05)  # Assuming a 5% increase in savings each year
 #Output the results
