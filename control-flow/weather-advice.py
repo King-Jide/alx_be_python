a36848d425d7371cd1831b3a8853c11a103abc16
David Jideofor <jide.remote@gmail.com>
2025-07-20 16:15:17 +0100

    Add weather-advice program

---
+++ control-flow/weather-advice.py
@@ -0,0 +1,22 @@
+while True:
+    #1. Ask user to input the current weather.
+    current_weather = input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
+
+    #2. Provide Clothing recommendations
+
+    if current_weather == "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    elif current_weather == "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+    elif current_weather == "cold":
+        print("Make sure to wear a warm coat and a scarf.")
+    # else:
+    #     print("Sorry, I don't have recommendations for this weather.")
+
+    #Ask to try again
+    play_again = input("Do you wish to get another recommendation? (y/n)").strip().lower()
+    if play_again != 'y':
+        print("Thanks for trying out our product")
+        break
+
+
