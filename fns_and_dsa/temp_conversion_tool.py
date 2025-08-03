4ba9411cbf958cdba9bade972444c0b1b4a0aca6
David Jideofor <jide.remote@gmail.com>
2025-08-03 15:42:58 +0100

    add temperature convertion tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,47 @@
+
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def main():
+    try:
+        #Ask the user to enter a temperature value
+        temp_input = input("Enter the temperature to convert: ")
+        #convert the input to a float (this will raise ValueError)
+        temperature = float(temp_input) 
+
+        #Ask the user to specify the temperature unit (C or F)
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+        if unit == 'C':
+            converted = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {converted:.2f}°F" )
+        elif unit == 'F':
+           converted = convert_to_celsius(temperature)
+           print(f"{temperature}°F is {converted:2f}°C")
+        else:
+            print("Invalid unit. Please enter 'C' or 'F'.")
+
+    except ValueError:
+        raise ValueError("Invalid temperature. Please enter a numeric value.")
+    
+#Ensures the main() function only runs if the script is executed directly
+if __name__ =="__main__":
+    main()
+
+
+        
+
+
+
+
+
+
+
