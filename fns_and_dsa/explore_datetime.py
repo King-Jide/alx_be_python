e4fbce7b71130b77db4513cf7d1b5fe44106b778
David Jideofor <jide.remote@gmail.com>
2025-08-03 14:56:00 +0100

    add Current date and time script

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current Date and Time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+def calculate_future_date():
+    try:
+        days=int(input("Enter the number of days into the future: "))
+        future_date = datetime.now() + timedelta(days=days)
+        print("Future Date:", future_date.strftime("%Y-%m-%d"))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+def main():
+    print("=== Date and Time Explorer ===")
+    display_current_datetime()
+    calculate_future_date()
+
+if __name__ == "__main__":
+    main()
