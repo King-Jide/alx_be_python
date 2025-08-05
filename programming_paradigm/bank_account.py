02b9f964a711a782d930bc45d658ed14ef1b2c52
David Jideofor <jide.remote@gmail.com>
2025-08-05 15:51:44 +0100

    add programming paradigm project

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+        
+    def deposit(self, amount):
+        self.account_balance += amount 
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+           print("Insufficient funds")
+           return False
+
+
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")
+    
+
+
+
+
+
+
