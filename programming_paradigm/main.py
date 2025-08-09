eea37a19305f40073141091c9f0d0bc7320f0ded
David Jideofor <jide.remote@gmail.com>
2025-08-09 11:22:34 +0100

    library management script

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,7 +1,8 @@
 import sys
 from robust_division_calculator import safe_divide
+from library_management import Book, Library
 
-def main():
+def run_division_calculator():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
@@ -16,8 +17,32 @@ def main():
         print("Division could not be performed")
 
 
+def run_library_demo():
+    # Setup a small library
+    library = Library()
+    library.add_book(Book("Brave New World", "Aldous Huxley"))
+    library.add_book(Book("1984", "George Orwell"))
+
+    # Initial list of available books
+    print("Available books after setup:")
+    library.list_available_books()
+
+    # Simulate checking out a book
+    library.check_out_book("1984")
+    print("\nAvailable books after checking out '1984':")
+    library.list_available_books()
+
+    # Simulate returning a book
+    library.return_book("1984")
+    print("\nAvailable books after returning '1984':")
+    library.list_available_books()
+
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == "library":
+        run_library_demo()
+    else:
+        run_division_calculator()
 
 
 
